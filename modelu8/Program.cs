5b0fbadf066c030f639007fc56532f9b5f256b9a
Asanlb <asan.ggg007@gmail.com>
2023-11-02 19:58:24 +0600

    Добавьте файлы проекта.

---
+++ modelu8/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+public class SquaredArray
+{
+    private double[] array;
+
+    public SquaredArray(int size)
+    {
+        array = new double[size];
+    }
+
+    public double this[int index]
+    {
+        get
+        {
+            if (IsIndexValid(index))
+            {
+                return array[index];
+            }
+            throw new IndexOutOfRangeException("Индекс вне диапазона.");
+        }
+        set
+        {
+            if (IsIndexValid(index))
+            {
+                array[index] = value * value; 
+            }
+            else
+            {
+                throw new IndexOutOfRangeException("Индекс вне диапазона.");
+            }
+        }
+    }
+
+    private bool IsIndexValid(int index)
+    {
+        return index >= 0 && index < array.Length;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SquaredArray array = new SquaredArray(5);
+        array[0] = 2; 
+        array[1] = 3; 
+        array[2] = 4; 
+
+        for (int i = 0; i < 3; i++)
+        {
+            Console.WriteLine($"array[{i}] = {array[i]}");
+        }
+    }
+}
